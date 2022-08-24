50a8988e12b1dfb0078b22753eea7b3518af1452
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-24 16:43:43 +0200

    add scroll to top wrapper to whole app

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,22 @@ import Explore from "./pages/Explore";
 import Creators from "./pages/Creators";
 import Community from "./pages/Community";
 import ErrorPage from "./pages/ErrorPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="explore" element={<Explore />} />
-        <Route path="creators" element={<Creators />} />
-        <Route path="community" element={<Community />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <ScrollToTop>
+        <GlobalStyle />
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="creators" element={<Creators />} />
+          <Route path="community" element={<Community />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </ScrollToTop>
     </ThemeProvider>
   );
 }

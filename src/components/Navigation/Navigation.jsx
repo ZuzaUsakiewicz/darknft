0ba0880937d81d8f628a3ecc550ec703319d8c90
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-24 16:54:55 +0200

    create responsive navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import {
+  Navbar,
+  NavbarList,
+  NavbarLink,
+  HamburgerButton,
+  ActionButton,
+} from "./Navigation.styled";
+import Logo from "../../assets/logo.svg";
+
+const Navigation = () => {
+  const [open, setOpen] = useState(false);
+  const [navBg, setNavBg] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const displayNavBg = () => {
+    if (window.scrollY >= 100) {
+      setNavBg(true);
+    } else {
+      setNavBg(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", displayNavBg);
+  }, []);
+
+  return (
+    <Navbar navBg={navBg}>
+      <NavbarLink to="/" onClick={() => setOpen(false)}>
+        <img src={Logo} alt="nft logo" />
+      </NavbarLink>
+      <NavbarList open={open} onClick={() => setOpen(false)}>
+        <li>
+          <NavbarLink to="/">Home</NavbarLink>
+        </li>
+        <li>
+          <NavbarLink to="/explore">Explore</NavbarLink>
+        </li>
+        <li>
+          <NavbarLink to="/creators">Creators</NavbarLink>
+        </li>
+        <li>
+          <NavbarLink to="/community">Community</NavbarLink>
+        </li>
+        <li>
+          <ActionButton>Connect Wallet</ActionButton>
+        </li>
+      </NavbarList>
+      <HamburgerButton onClick={() => toggleMenu()} open={open}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </HamburgerButton>
+    </Navbar>
+  );
+};
+
+export default Navigation;

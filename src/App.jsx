0605fcbba3038f986ac658cada2ff42e891c1787
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-24 16:38:17 +0200

    add app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./theme/globalStyles";
 import { theme } from "./theme/theme";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import Creators from "./pages/Creators";
+import Community from "./pages/Community";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <h1>Nothing</h1>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="explore" element={<Explore />} />
+        <Route path="creators" element={<Creators />} />
+        <Route path="community" element={<Community />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </ThemeProvider>
   );
 }

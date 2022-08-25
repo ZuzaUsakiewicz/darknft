f982843cbf970b0a2a3fb9be17d4f5d9674551e7
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-25 20:06:33 +0200

    add basic color variables

---
+++ src/theme/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  backgroundColor: "#1c121f",
+  bodyTextColor: "#F5FBF2",
+  secondaryBodyTextColor: "#9B9E9A",
+  accentColor: "#FFCE4E",
+  cardBackgroundColor: "#3D2942",
+  cardButtonBgColor: "#4E3A4C",
+  buttonBgColor: "##2B1E29",
+};

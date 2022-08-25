c1dfc054d2066db755b4dc69c25d3f3bbfcceb3d
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-25 20:06:53 +0200

    add basic typography variables

---
+++ src/theme/typography.js
@@ -0,0 +1,15 @@
+export const typography = {
+  // fonts:
+  mainFont: "'Oxanium', cursive",
+  secondaryFont: "'Work Sans', sans-serif",
+
+  //font-weights:
+  regularFW: 400,
+  mediumFW: 500,
+  semiBoldFW: 600,
+  boldFW: 700,
+
+  // font-sizes:
+  heroHeading: "4.0625rem",
+  linkFS: "1.125rem",
+};

2d5d499d6536a75f9e58501cc76621a625a314ab
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-27 17:45:22 +0200

    create nft data context

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, createContext } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./theme/globalStyles";
 import { theme } from "./theme/theme";
@@ -9,21 +10,28 @@ import Creators from "./pages/Creators";
 import Community from "./pages/Community";
 import ErrorPage from "./pages/ErrorPage";
 import ScrollToTop from "./components/ScrollToTop";
+import nftdata from "./nftdata";
+
+export const NftContext = createContext();
 
 function App() {
+  const [data] = useState(nftdata);
+
   return (
     <ThemeProvider theme={theme}>
-      <ScrollToTop>
-        <GlobalStyle />
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="creators" element={<Creators />} />
-          <Route path="community" element={<Community />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </ScrollToTop>
+      <NftContext.Provider value={data}>
+        <ScrollToTop>
+          <GlobalStyle />
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="explore" element={<Explore />} />
+            <Route path="creators" element={<Creators />} />
+            <Route path="community" element={<Community />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </ScrollToTop>
+      </NftContext.Provider>
     </ThemeProvider>
   );
 }

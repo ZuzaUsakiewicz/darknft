d60de392e6129897d67c5581653054addc3abe08
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-08-31 19:11:26 +0200

    create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "../components/HomePage/HeroSection";
+import ClientsLogos from "../components/HomePage/ClientsLogos";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ClientsLogos />
+    </>
+  );
+}
+
+export default Home;

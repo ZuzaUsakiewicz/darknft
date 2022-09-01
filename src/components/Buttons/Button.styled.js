c6e98e9d1428e2b2066df93f22c752637f046280
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-01 20:40:22 +0200

    extend buttons styles

--- src/components/Buttons/Button.styled.js
+++ src/components/Buttons/Button.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { mediaQueries } from "../../theme/mediaQueries";
 
 export const Button = styled(Link)`
   padding: 1rem 2.875rem;
@@ -7,9 +8,23 @@ export const Button = styled(Link)`
   background: ${({ primary, theme }) =>
     primary ? theme.colors.accentColor : "rgb(89, 72, 70)"};
   color: ${({ primary, theme }) =>
-    primary ? theme.colors.backgroundColor : theme.colors.bodyTextColor};
+    primary ? theme.colors.buttonTextColor : theme.colors.bodyTextColor};
   cursor: pointer;
   text-decoration: none;
+  font-family: ${({ theme }) => theme.typography.secondaryFont};
   font-weight: 600;
   text-transform: capitalize;
+  letter-spacing: 1px;
+  @media ${mediaQueries.laptop} {
+    font-size: 1.5rem;
+    padding: 1.2rem 4rem;
+  }
+`;
+
+export const CardButton = styled(Button)`
+  padding: 0.8rem 4rem;
+  width: 100%;
+  font-size: 1.25rem;
+  background: ${({ theme }) => theme.colors.cardButtonBgColor};
+  border: 1px solid #bcb39a;
 `;

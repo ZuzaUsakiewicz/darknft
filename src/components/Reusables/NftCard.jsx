ba285c4bcb7df9be9ca7418f97543d4eca572464
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-02 21:08:00 +0200

    create life auctions section nft card component

---
+++ src/components/Reusables/NftCard.jsx
@@ -0,0 +1,39 @@
+import { CardButton } from "../Buttons/Button.styled";
+import {
+  CardContainer,
+  ImageContainer,
+  InfoContainer,
+  Avatar,
+  CardInfo,
+  Loved,
+  CurrentBidContainer,
+} from "./NftCard.styled";
+import heart from "../../assets/heart.svg";
+
+const NftCard = ({ item }) => {
+  return (
+    <CardContainer>
+      <ImageContainer>
+        <img src={item.image} />
+      </ImageContainer>
+      <InfoContainer>
+        <Avatar></Avatar>
+        <CardInfo>
+          <h5>{item.category}</h5>
+          <p> by @{item.author}</p>
+        </CardInfo>
+        <Loved>
+          <img src={heart} />
+          <h4>{item.loved}</h4>
+        </Loved>
+      </InfoContainer>
+      <CurrentBidContainer>
+        <h3>Current Bid</h3>
+        <span>{item.currentBid} ETH</span>
+      </CurrentBidContainer>
+      <CardButton to="/">Place a bid</CardButton>
+    </CardContainer>
+  );
+};
+
+export default NftCard;

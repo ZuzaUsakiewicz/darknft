79e9b4e809c9617f7fb4785ba6ca84715751088c
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-06 20:55:48 +0200

    create theme imports

---
+++ src/theme/theme.js
@@ -0,0 +1,9 @@
+import { colors } from "./colors";
+import { typography } from "./typography";
+import { mediaQueries } from "./mediaQueries";
+
+export const theme = {
+  colors,
+  typography,
+  mediaQueries,
+};

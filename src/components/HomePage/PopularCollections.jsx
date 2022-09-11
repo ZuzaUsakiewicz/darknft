f2fd69f4299e54481d9b0fb76156efc6e225d4fc
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-11 20:25:32 +0200

    create popular collections section

---
+++ src/components/HomePage/PopularCollections.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { SectionContainer } from "../Reusables/Container.styled";
+import { SectionTitle } from "../Reusables/SectionTitle.styled";
+import { NftContext } from "../../App";
+import {
+  CollectionsContainer,
+  CollectionCard,
+  CardInfo,
+} from "./PopularCollections.styled";
+import { Avatar } from "../Reusables/NftCard.styled";
+import { Button } from "../Buttons/Button.styled";
+
+const PopularCollections = () => {
+  const data = useContext(NftContext);
+  let collections = [
+    ...new Map(data.map((item) => [item["author"], item])).values(),
+  ].slice(0, 3);
+  let categories = [
+    ...new Map(data.map((item) => [item["category"], item])).values(),
+  ];
+
+  return (
+    <SectionContainer>
+      <SectionTitle color="#E03054">Popular Collections</SectionTitle>
+      <CollectionsContainer>
+        {collections.map((collection, index) => {
+          return (
+            <CollectionCard key={index}>
+              <img src={collection.image} />
+              <CardInfo>
+                <span>
+                  <Avatar />
+                  <h3>@{collection.author}</h3>
+                </span>
+                <p>ERC-721</p>
+              </CardInfo>
+            </CollectionCard>
+          );
+        })}
+        {console.log(categories)}
+      </CollectionsContainer>
+      <Button to="/explore">view all</Button>
+    </SectionContainer>
+  );
+};
+
+export default PopularCollections;

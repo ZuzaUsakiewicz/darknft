29d33bf60a3d5d370d93aa75ba6516bfea3b2a20
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-13 19:03:16 +0200

    add top creators section styles

---
+++ src/components/HomePage/TopCreatorsSection.styled.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import {
+  flexColumnCenter,
+  flexRowCenter,
+  flexRowSpaceBetween,
+} from "../Reusables/styledHelpers";
+import { mediaQueries } from "../../theme/mediaQueries";
+
+export const CreatorsContainer = styled.div`
+  ${flexColumnCenter};
+  gap: 1rem;
+  padding: 3rem 0;
+  @media ${mediaQueries.tablet} {
+    gap: 2rem;
+    padding: 4rem 5rem;
+    max-width: 58rem;
+    ${flexRowCenter};
+    flex-wrap: wrap;
+  }
+`;
+export const CreatorContainer = styled.div`
+  width: 20rem;
+  height: 6rem;
+  ${flexRowSpaceBetween};
+  padding: 0 0.8rem;
+  background: #302034;
+  position: relative;
+  @media ${mediaQueries.laptop} {
+    width: 23rem;
+  }
+
+  h3 {
+    font-family: ${({ theme }) => theme.typography.mainFont};
+    font-size: 1.25rem;
+  }
+  h4 {
+    font-family: ${({ theme }) => theme.typography.secondaryFont};
+    font-size: 1rem;
+    color: ${({ theme }) => theme.colors.footerColor};
+    font-weight: 500;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: -0.1rem;
+    bottom: 0;
+    width: 0.1rem;
+    height: 100%;
+    background: #e03054;
+  }
+`;
+
+export const Name = styled.div`
+  width: 55%;
+  text-align: left;
+`;
+
+export const Items = styled.div`
+  width: 20%;
+  position: relative;
+  /* padding: 0 0.5rem; */
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0.1rem;
+    left: -0.6rem;
+    bottom: 0.1rem;
+    width: 0.1rem;
+    height: 100%;
+    background: #5e4564;
+  }
+`;

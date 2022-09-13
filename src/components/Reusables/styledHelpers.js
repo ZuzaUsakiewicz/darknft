330b21258001aecbfcd2473d981b4e9cded9bcda
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-13 19:02:29 +0200

    create flexbox styled helpers

---
+++ src/components/Reusables/styledHelpers.js
@@ -0,0 +1,27 @@
+import { css } from "styled-components";
+
+export const flexRowCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+export const flexRowSpaceBetween = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+export const flexColumnCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const flexColumnAlignLeft = css`
+  display: flex;
+  justify-content: center;
+  align-items: left;
+  flex-direction: column;
+`;

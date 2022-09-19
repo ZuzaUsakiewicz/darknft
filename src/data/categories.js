85c2a0c79f70368318c11de71cad67368ce3e493
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-19 19:44:57 +0200

    create categories data

---
+++ src/data/categories.js
@@ -0,0 +1,34 @@
+const categories = [
+  {
+    id: 0,
+    name: "music",
+    image: "src/assets/photos/cat-music.jpg",
+  },
+  {
+    id: 1,
+    name: "art",
+    image: "src/assets/photos/cat-art.jpg",
+  },
+  {
+    id: 2,
+    name: "entertainment",
+    image: "src/assets/photos/cat-entertainment.jpg",
+  },
+  {
+    id: 3,
+    name: "gaming",
+    image: "src/assets/photos/cat-gaming.jpg",
+  },
+  {
+    id: 4,
+    name: "literature",
+    image: "src/assets/photos/cat-literature.jpg",
+  },
+  {
+    id: 5,
+    name: "goods",
+    image: "src/assets/photos/cat-goods.jpg",
+  },
+];
+
+export default categories;

4dd872b66adec4419ef344035cc5d39be23b03ae
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-19 17:45:54 +0200

    add box-shadow mixin

--- src/components/Reusables/styledHelpers.js
+++ src/components/Reusables/styledHelpers.js
@@ -25,3 +25,12 @@ export const flexColumnAlignLeft = css`
   align-items: left;
   flex-direction: column;
 `;
+
+export const CardShadow = css`
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1), 0px 2px 4px rgba(0, 0, 0, 0.1),
+    0px 4px 8px rgba(0, 0, 0, 0.1), 0px 8px 16px rgba(0, 0, 0, 0.1);
+`;
+export const CardShadowHover = css`
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.4), 0px 2px 4px rgba(0, 0, 0, 0.4),
+    0px 4px 8px rgba(0, 0, 0, 0.4), 0px 8px 16px rgba(0, 0, 0, 0.4);
+`;

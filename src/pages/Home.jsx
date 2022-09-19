2b727cadac43a26e6ee31730889153acaff83c33
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-19 19:40:03 +0200

    add sections components to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import HeroSection from "../components/HomePage/HeroSection";
 import ClientsLogos from "../components/HomePage/ClientsLogos";
+import LiveAuctions from "../components/HomePage/LiveAuctions";
+import HowItWorks from "../components/HomePage/HowItWorks";
+import PopularCollections from "../components/HomePage/PopularCollections";
+import Newsletter from "../components/HomePage/Newsletter";
+import TopCreatorsSection from "../components/HomePage/TopCreatorsSection";
+import ExploreByCategories from "../components/HomePage/ExploreByCategories";
 
 function Home() {
   return (
     <>
       <HeroSection />
       <ClientsLogos />
+      <LiveAuctions />
+      <HowItWorks />
+      <PopularCollections />
+      <ExploreByCategories />
+      <TopCreatorsSection />
+      <Newsletter />
     </>
   );
 }

bd3901e28bf9938046dbd6a8dcfeb4bee7c5f18a
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-19 19:39:30 +0200

    add new colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -2,8 +2,11 @@ export const colors = {
   backgroundColor: "#1c121f",
   bodyTextColor: "#F5FBF2",
   secondaryBodyTextColor: "#9B9E9A",
+  buttonTextColor: "#16091B",
   accentColor: "#FFCE4E",
   cardBackgroundColor: "#3D2942",
   cardButtonBgColor: "#4E3A4C",
-  buttonBgColor: "##2B1E29",
+  buttonBgColor: "#2b1e29",
+  footerColor: "#96839B",
+  footerIconsColor: "#D53D5C",
 };

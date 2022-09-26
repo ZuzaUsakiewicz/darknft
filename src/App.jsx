bbb049d0bca9946dd7056096adc12e8d9fe35503
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-26 18:24:33 +0200

    implement lazy loading for Explore Page with Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState, createContext } from "react";
+import React, { useState, createContext, Suspense } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./theme/globalStyles";
 import { theme } from "./theme/theme";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home";
-import Explore from "./pages/Explore";
 import Creators from "./pages/Creators";
 import Community from "./pages/Community";
 import ScrollToTop from "./components/ScrollToTop";
@@ -13,6 +12,8 @@ import nftdata from "./data/nftdata";
 import Page404 from "./components/404/Page404";
 import Footer from "./components/Footer/Footer";
 import { AllAuctions } from "./components/ExplorePage/AllAuctions";
+
+const Explore = React.lazy(() => import("./pages/Explore"));
 
 export const NftContext = createContext();
 
@@ -27,7 +28,14 @@ function App() {
           <Navigation />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="explore/" element={<Explore />}>
+            <Route
+              path="explore/"
+              element={
+                <React.Suspense fallback={<div>Loading...</div>}>
+                  <Explore />
+                </React.Suspense>
+              }
+            >
               <Route index element={<AllAuctions />} />
               <Route path="*" element={<Page404 />} />
             </Route>

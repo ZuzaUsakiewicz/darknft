c9be3e06d11deb13896a823b54162c9790f52692
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-09-26 18:26:46 +0200

    replace nft card image with lazy load image

--- src/components/Reusables/NftCard.jsx
+++ src/components/Reusables/NftCard.jsx
@@ -9,21 +9,28 @@ import {
 } from "./NftCard.styled";
 import heart from "../../assets/icons/heart.svg";
 import Avatar from "../Reusables/Avatar";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const NftCard = ({ item }) => {
   return (
     <CardContainer>
       <ImageContainer>
-        <img src={item.image} />
+        <LazyLoadImage
+          src={item.image}
+          alt="nft product image"
+          effect="opacity"
+          width="100%"
+          height="304px"
+        />
       </ImageContainer>
       <InfoContainer>
-        <Avatar imgSrc={item.image} />
+        <Avatar imgSrc={item.image} alt="creator avatar" />
         <CardInfo>
           <h5>{item.name}</h5>
           <p> by @{item.author}</p>
         </CardInfo>
         <Loved>
-          <img src={heart} />
+          <img src={heart} alt="heart logo" />
           <h4>{item.loved > 999 ? "999+" : item.loved}</h4>
         </Loved>
       </InfoContainer>

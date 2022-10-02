7a5d659a48bc6064feaec658ab4ec411d4ec1d12
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-10-02 20:28:29 +0200

    create Animate Presence provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, createContext, Suspense } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./theme/globalStyles";
 import { theme } from "./theme/theme";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home";
 import Creators from "./pages/Creators";
@@ -12,6 +12,8 @@ import nftdata from "./data/nftdata";
 import Page404 from "./components/404/Page404";
 import Footer from "./components/Footer/Footer";
 import { AllAuctions } from "./components/ExplorePage/AllAuctions";
+import LoadingSection from "./components/Reusables/LoadingSection";
+import { AnimatePresence } from "framer-motion";
 
 const Explore = React.lazy(() => import("./pages/Explore"));
 
@@ -19,6 +21,7 @@ export const NftContext = createContext();
 
 function App() {
   const [data] = useState(nftdata);
+  const location = useLocation();
 
   return (
     <ThemeProvider theme={theme}>
@@ -26,23 +29,25 @@ function App() {
         <ScrollToTop>
           <GlobalStyle />
           <Navigation />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="explore/"
-              element={
-                <React.Suspense fallback={<div>Loading...</div>}>
-                  <Explore />
-                </React.Suspense>
-              }
-            >
-              <Route index element={<AllAuctions />} />
+          <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="explore/"
+                element={
+                  <React.Suspense fallback={<LoadingSection />}>
+                    <Explore />
+                  </React.Suspense>
+                }
+              >
+                <Route index element={<AllAuctions />} />
+                <Route path="*" element={<Page404 />} />
+              </Route>
+              <Route path="creators" element={<Creators />} />
+              <Route path="community" element={<Community />} />
               <Route path="*" element={<Page404 />} />
-            </Route>
-            <Route path="creators" element={<Creators />} />
-            <Route path="community" element={<Community />} />
-            <Route path="*" element={<Page404 />} />
-          </Routes>
+            </Routes>
+          </AnimatePresence>
           <Footer />
         </ScrollToTop>
       </NftContext.Provider>

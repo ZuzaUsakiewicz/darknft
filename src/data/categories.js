8428b600a560c22194091ea0e2bd5f29654efa94
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-10-05 10:08:34 +0200

    update images source

--- src/data/categories.js
+++ src/data/categories.js
@@ -2,32 +2,32 @@ const categories = [
   {
     id: 0,
     name: "music",
-    image: "src/assets/photos/cat-music.jpg",
+    image: "../src/assets/photos/cat-music.jpg",
   },
   {
     id: 1,
     name: "art",
-    image: "src/assets/photos/cat-art.jpg",
+    image: "../src/assets/photos/cat-art.jpg",
   },
   {
     id: 2,
     name: "entertainment",
-    image: "src/assets/photos/cat-entertainment.jpg",
+    image: "../src/assets/photos/cat-entertainment.jpg",
   },
   {
     id: 3,
     name: "gaming",
-    image: "src/assets/photos/cat-gaming.jpg",
+    image: "../src/assets/photos/cat-gaming.jpg",
   },
   {
     id: 4,
     name: "literature",
-    image: "src/assets/photos/cat-literature.jpg",
+    image: "../src/assets/photos/cat-literature.jpg",
   },
   {
     id: 5,
     name: "goods",
-    image: "src/assets/photos/cat-goods.jpg",
+    image: "../src/assets/photos/cat-goods.jpg",
   },
 ];
 

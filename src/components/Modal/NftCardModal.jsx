44608f9398ebf2fc7a696c81891e66cb9e8da868
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-10-06 11:54:23 +0200

    add nft card modal content

--- src/components/Modal/NftCardModal.jsx
+++ src/components/Modal/NftCardModal.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { motion } from "framer-motion";
+import {
+  CardModalContainer,
+  ImgContainer,
+  InfoContainer,
+  CollectButton,
+} from "./NftCardModal.styled";
+import { SiEthereum } from "react-icons/si";
 
 const NftCardModal = ({ item }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50, scale: 0.3 }}
+      initial={{ opacity: 0, y: 50, scale: 0.2 }}
       animate={{ opacity: 1, y: 0, scale: 1 }}
-      exit={{ opacity: 0, y: 20, scale: 0.5, transition: { delay: 1 } }}
+      exit={{ opacity: 0, y: 20, scale: 0.2, transition: { delay: 1 } }}
     >
-      {item.name}
+      <CardModalContainer>
+        <ImgContainer>
+          <img src={item.image} />
+        </ImgContainer>
+        <InfoContainer>
+          <div>
+            <p>
+              <span>{item.name}</span> by {item.author}
+            </p>
+            <p>
+              <SiEthereum />
+              {item.currentBid} ETH
+            </p>
+          </div>
+          <CollectButton to={`/explore/?category=${item.category}`}>
+            Collect!
+          </CollectButton>
+        </InfoContainer>
+      </CardModalContainer>
     </motion.div>
   );
 };

ea137efb8cfde9d9c604ef2b6381a40bde339c23
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-10-28 12:07:57 +0200

    refactor: add information about page purpose

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -3,11 +3,24 @@ import LoadingSection from "../components/Reusables/LoadingSection";
 
 function Community() {
   return (
-    <LoadingSection
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    />
+    <>
+      <LoadingSection
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      />
+      <p
+        style={{
+          position: "absolute",
+          top: "20%",
+          left: "50%",
+          fontSize: "12px",
+          transform: "translate(-50%, 0)",
+        }}
+      >
+        *Nothing's wrong here. I've made that page to show loading indicator.
+      </p>
+    </>
   );
 }
 
